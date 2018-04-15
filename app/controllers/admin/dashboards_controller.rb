66143974288258b8fcedabbd90b94f14e4addbb3
Petr Schmied <peter9209@gmail.com>
2018-04-15 22:14:12 +0200

    Talent trees, artifact items & titles preloading

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -4,36 +4,66 @@ class Admin::DashboardsController < ApplicationController
   def index
     # Init
     unless CharacterClass.any?
+      courses = Kos.get_courses(session[:user]['token'])
+
+      # Classes
+
       class_bc = CharacterClass.create(name: 'Student FIT (Bc)', code: 'BI')
       class_ing = CharacterClass.create(name: 'Student FIT (Ing)', code: 'MI')
 
-      courses = Kos.get_courses(session[:user]['token'])
-
-      # Specializations & Talents
+      # Specializations, talents & talent trees
 
       courses[:branch]['BI'].each do |branch, data|
         spec = class_bc.specializations.find_or_create_by(name: branch, code: data['id'],
                                                           abbr: data['code'])
+        tree = spec.create_talent_tree(width: 1920, height: 1080, talent_size: 50)
         data['courses'].each do |course|
           talent = Talent.find_or_create_by(
             name: course['name'], description: course['description'],
             points: course['credits'], code: course['code'])
           spec.talents << talent
+          tree.add_talent(talent, 0, 0)
         end
+        tree.save
         spec.save
       end
 
       courses[:branch]['MI'].each do |branch, data|
         spec = class_ing.specializations.find_or_create_by(name: branch, code: data['id'],
                                                            abbr: data['code'])
+        tree = spec.create_talent_tree(width: 1920, height: 1080, talent_size: 50)
         data['courses'].each do |course|
           talent = Talent.find_or_create_by(
             name: course['name'], description: course['description'],
             points: course['credits'], code: course['code'])
           spec.talents << talent
+          tree.add_talent(talent, 0, 0)
         end
+        tree.save
         spec.save
       end
+
+      # Talent trees for classes
+
+      tree_bc = class_bc.create_talent_tree(width: 1920, height: 1080, talent_size: 50)
+      tree_ing = class_ing.create_talent_tree(width: 1920, height: 1080, talent_size: 50)
+
+      courses[:programme]['BI'].each do |course|
+        talent = Talent.find_or_create_by(
+          name: course['name'], description: course['description'],
+          points: course['credits'], code: course['code'])
+        tree_bc.add_talent(talent, 0, 0)
+      end
+
+      courses[:programme]['MI'].each do |course|
+        talent = Talent.find_or_create_by(
+          name: course['name'], description: course['description'],
+          points: course['credits'], code: course['code'])
+        tree_ing.add_talent(talent, 0, 0)
+      end
+
+      tree_bc.save
+      tree_ing.save
 
       # Achievement categories
       courses_category = AchievementCategory.create(name: 'Předměty')
@@ -49,6 +79,69 @@ class Admin::DashboardsController < ApplicationController
       courses[:programme]['MI'].each do |course|
         courses_mi_category.subcategories.create(name: course['name'])
       end
+
+      # Items & their talent trees
+
+      artifact_bc = Item.create(name: "#{class_bc.name} - artefakt",
+                                rarity: 'artifact')
+      artifact_ing = Item.create(name: "#{class_ing.name} - artefakt",
+                                 rarity: 'artifact')
+
+      artifact_bc_tree = artifact_bc.create_talent_tree(
+        width: 1920, height: 1080, talent_size: 50)
+      artifact_ing_tree = artifact_ing.create_talent_tree(
+        width: 1920, height: 1080, talent_size: 50)
+
+      courses[:programme]['BI'].each do |course|
+        talent = Talent.find_or_create_by(
+          name: course['name'], description: course['description'],
+          points: course['credits'], code: course['code'])
+        artifact_bc_tree.add_talent(talent, 0, 0)
+      end
+
+      courses[:programme]['MI'].each do |course|
+        talent = Talent.find_or_create_by(
+          name: course['name'], description: course['description'],
+          points: course['credits'], code: course['code'])
+        artifact_ing_tree.add_talent(talent, 0, 0)
+      end
+
+      courses[:branch]['BI'].each do |branch, data|
+        artifact_spec = Item.find_or_create_by(
+          name: "#{branch} (BI) - artefakt",
+          rarity: 'artifact')
+        tree_artifact_spec = artifact_spec.create_talent_tree(
+          width: 1920, height: 1080, talent_size: 50)
+
+        data['courses'].each do |course|
+          talent = Talent.find_or_create_by(
+            name: course['name'], description: course['description'],
+            points: course['credits'], code: course['code'])
+          tree_artifact_spec.add_talent(talent, 0, 0)
+        end
+        tree_artifact_spec.save
+      end
+
+      courses[:branch]['MI'].each do |branch, data|
+        artifact_spec = Item.find_or_create_by(
+          name: "#{branch} (MI) - artefakt",
+          rarity: 'artifact')
+        tree_artifact_spec = artifact_spec.create_talent_tree(
+          width: 1920, height: 1080, talent_size: 50)
+
+        data['courses'].each do |course|
+          talent = Talent.find_or_create_by(
+            name: course['name'], description: course['description'],
+            points: course['credits'], code: course['code'])
+          tree_artifact_spec.add_talent(talent, 0, 0)
+        end
+        tree_artifact_spec.save
+      end
+
+      # Titles
+
+      Title.create(title: 'Bc.', after_name: false)
+      Title.create(title: 'Ing.', after_name: false)
     end
   end
 end
--- app/models/talent_tree.rb
+++ app/models/talent_tree.rb
@@ -1,7 +1,11 @@
 class TalentTree < ApplicationRecord
-  belongs_to :character_class
-  belongs_to :specialization
+  belongs_to :character_class, optional: true
+  belongs_to :specialization, optional: true
   belongs_to :item, optional: true
   has_many :talent_tree_talents
   has_many :talents, through: :talent_tree_talents
+
+  def add_talent(talent, x, y)
+    self.talent_tree_talents.build(talent: talent, pos_x: x, pos_y: y)
+  end
 end
