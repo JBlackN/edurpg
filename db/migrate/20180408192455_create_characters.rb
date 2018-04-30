60c4426a09e19fa34c537208fb3d6c9f14e912fe
Petr Schmied <peter9209@gmail.com>
2018-04-30 23:45:24 +0200

    Cleanup: health

--- db/migrate/20180408192455_create_characters.rb
+++ db/migrate/20180408192455_create_characters.rb
@@ -3,7 +3,6 @@ class CreateCharacters < ActiveRecord::Migration[5.1]
     create_table :characters do |t|
       t.string :name
       t.text :image
-      t.integer :health
       t.integer :experience
       t.integer :level
       t.references :user, foreign_key: true
