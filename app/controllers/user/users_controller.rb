cb3f446362c5aa0a08ecedd87eab57d7f8dfbd9b
Petr Schmied <peter9209@gmail.com>
2018-04-30 22:22:00 +0200

    User: data export

--- app/controllers/user/users_controller.rb
+++ app/controllers/user/users_controller.rb
@@ -1,6 +1,14 @@
 class User::UsersController < ApplicationController
   before_action :authorize_user
 
+  def show
+    respond_to do |format|
+      format.html { redirect_to edit_user_user_path(current_user) }
+      format.json { send_data data_for_export,
+                    type: :json, disposition: 'attachment' }
+    end
+  end
+
   def edit
     @character = current_user.character
   end
@@ -15,7 +23,7 @@ class User::UsersController < ApplicationController
     end
 
     if @character.save
-      redirect_to edit_admin_user_path(current_user)
+      redirect_to edit_user_user_path(current_user)
     else
       render 'edit' # TODO: errors -> view
     end
@@ -24,5 +32,76 @@ class User::UsersController < ApplicationController
   def destroy
     current_user.destroy
     redirect_to :logout
+  end
+
+  private
+
+  def data_for_export
+    data = current_user.to_json(
+      include: [
+        :permission,
+        :consents,
+        {
+          character: {
+            include: [
+              :character_class,
+              :specialization,
+              {
+                completed_quests: {
+                  include: [
+                    :character_class,
+                    :specialization,
+                    :talent,
+                    :skills,
+                    :achievements,
+                    :items,
+                    :titles
+                  ]
+                }
+              },
+              {
+                talent_trees: {
+                  include: {
+                    talent_tree_talents: {
+                      include: :talent
+                    }
+                  }
+                }
+              },
+              {
+                character_character_attributes: {
+                  include: :character_attribute
+                }
+              },
+              {
+                character_skills: {
+                  include: :skill
+                }
+              },
+              {
+                character_achievements: {
+                  include: {
+                    achievements: {
+                      include: [:items, :titles]
+                    }
+                  }
+                }
+              },
+              {
+                character_items: {
+                  include: :item
+                }
+              },
+              {
+                character_titles: {
+                  include: :title
+                }
+              }
+            ]
+          }
+        }
+      ]
+    )
+    data
   end
 end
